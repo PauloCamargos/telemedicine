29783a0265894bd9bf42a499adeb57fcde22b9b3
PauloCamargos <paulo.camargos@ufu.br>
2018-05-26 19:15:38 -0300

    Register form using wtforms

--- html/forms.py
+++ html/forms.py
@@ -1,64 +1,11 @@
 # -*- coding: utf-8 -*-
-
+from constants import *
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, DateField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 # Documentation http://wtforms.simplecodes.com/docs/0.6.1/fields.html
-specialties  = ["Acupuntura",
-"Alergia e Imunologia",
-"Anestesiologia",
-"Angiologia",
-"Cancerologia",
-"Cardiologia",
-"Cirurgia Cardiovascular",
-"Cirurgia da Mão",
-"Cirurgia de Cabeça e Pescoço",
-"Cirurgia do Aparelho Digestivo",
-"Cirurgia Geral",
-"Cirurgia Pediátrica",
-"Cirurgia Plástica",
-"Cirurgia Torácica",
-"Cirurgia Vascular",
-"Clínica Médica",
-"Coloproctologia",
-"Dermatologia",
-"Endocrinologia e Metabologia",
-"Endoscopia",
-"Gastroenterologia",
-"Genética Médica",
-"Geral",
-"Geriatria",
-"Ginecologia e Obstetrícia",
-"Hematologia e Hemoterapia",
-"Homeopatia",
-"Infectologia",
-"Mastologia",
-"Medicina de Família e Comunidade",
-"Medicina do Trabalho",
-"Medicina de Tráfego",
-"Medicina Esportiva",
-"Medicina Física e Reabilitação",
-"Medicina Intensiva",
-"Medicina Legal e Perícia Médica",
-"Medicina Nuclear",
-"Medicina Preventiva e Social",
-"Nefrologia",
-"Neurocirurgia",
-"Neurologia",
-"Nutrologia",
-"Oftalmologia",
-"Ortopedia e Traumatologia",
-"Otorrinolaringologia",
-"Patologia",
-"Patologia Clínica/Medicina Laboratorial",
-"Pediatria",
-"Pneumologia",
-"Psiquiatria",
-"Radiologia e Diagnóstico por Imagem",
-"Radioterapia",
-"Reumatologia",
-"Urologia"]
 
+# specialties = constants.specialties
 
 # Class for registration
 class DoctorRegistrationForm(FlaskForm):
@@ -68,26 +15,30 @@ class DoctorRegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confrime a senha', validators=[DataRequired(), Length(min=6, max=32), EqualTo('Password')])
     crm = IntegerField('CRM', validators=[DataRequired()])
     fullname = StringField('Nome completo', validators=[DataRequired()])
-    birthdate = DateField('Data de nascimento', validators=[DataRequired()])
+    birthdate = DateField('Data de nascimento', validators=[DataRequired()], format="%d-%m-%Y")
     birthcity = StringField('Cidade de nascimento')
-    birthstate = StringField('Estado de nascimento')
+    # full_state = states.values()
+    birthstate = SelectField('Estado de nascimento', choices=states_dict.items())
     rg = StringField('RG', validators=[DataRequired()])
     cpf = StringField('CPF', validators=[DataRequired(), Length(min=10, max=12)])
     cep = IntegerField('CEP')
-    place = SelectField('Logradouro', choices=specialties)
+    place = SelectField('Logradouro', choices=logradouro_dict.items())
     residence_address = StringField('Endereço residencial')
     neighborhood = StringField('Bairro')
     city = StringField('Cidade')
+    state = SelectField('Estado', choices=states_dict.items())
     phone_1 = IntegerField('Telefone fixo')
     phone_2 = IntegerField('Telefone celular')
+    specialty = SelectField('Especialidade', choices=specialties_dict.items())
     # Clinic infos
     business_name = StringField('Razão Social')
     social_name = StringField('Nome fantasia')
     clinic_cep = IntegerField('CEP')
-    clinic_place = SelectField('Logradouro', choices=specialties)
+    clinic_place = SelectField('Logradouro', choices=logradouro_dict.items())
     clinic_residence_address = StringField('Endereço residencial')
     clinic_neighborhood = StringField('Bairro')
     clinic_city = StringField('Cidade')
+    clinic_state = SelectField('Estado', choices=states_dict.items())
     clinic_phone_1 = IntegerField('Telefone fixo')
     clinic_phone_2 = IntegerField('Telefone celular')
     clinic_email = StringField('Email', validators=[Email()])
