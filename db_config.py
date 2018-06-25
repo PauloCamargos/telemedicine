00d05832383152c5ab46b1905d7a2bc12a813a11
Ítalo Fernandes <italogsfernandes@gmail.com>
2018-06-25 07:24:22 -0300

    q bonito colocando o banco de dados no git kkkk

--- db_config.py
+++ db_config.py
@@ -21,6 +21,60 @@ italo = User(email='italo@hotmail.com',
              password=hashed_password, crm='4321',
              fullname='Italo Fernandes', rg='321',
              cpf='321', phone_1='92102109', cep='312321')
+
 db.session.add(italo)
 db.session.add(paulo)
+db.session.commit()
+
+especialista = User(email='especialista@gmail.com',
+             password=hashed_password, crm='0001',
+             fullname='Especialista', rg='001',
+             cpf='001', phone_1='0000001', cep='312321')
+generalista = User(email='generalista@gmail.com',
+             password=hashed_password, crm='0002',
+             fullname='Generalista', rg='002',
+             cpf='002', phone_1='0000002', cep='312321')
+
+db.session.add(especialista)
+db.session.add(generalista)
+db.session.commit()
+
+neurologia = Specialty.query.filter_by(specialty='Neurologia').first()
+geral = Specialty.query.filter_by(specialty='Geral').first()
+neurologia.doctors.append(especialista)
+geral.doctors.append(generalista)
+
+db.session.commit()
+
+apostolos = """Pedro , André, Zebedeu, João, Filipe, Bartolomeu, Tomé, Mateus, Tiago, Tadeu, Simão, Judas"""
+apostolos = [ apostolo.split()[0] for apostolo in apostolos.split(', ')]
+sobrenomes = """– Agostinho
+– Aguiar
+– Albuquerque
+– Alegria
+– Alencastro
+– Almada
+– Alves
+– Alvim
+– Amorim
+– Andrade
+– Antunes
+– Aparício
+– Apolinário
+– Araújo
+– Arruda
+– Assis
+– Assunção
+– Ávila
+– Azambuja"""
+sobrenomes = [sobrenome.split()[1] for sobrenome in sobrenomes.split('\n')]
+usuarios = []
+from random import randint
+for apostolo in apostolos:
+    u = User(email= apostolo.lower() + '@jesus.nazare',
+             password=hashed_password, crm='0002',
+             fullname=apostolo+sobrenomes[randint(0,len(sobrenomes)-1)], rg='002',
+             cpf='002', phone_1='0000002', cep='312321')
+    usuarios.append(u)
+    db.session.add(u)
 db.session.commit()
